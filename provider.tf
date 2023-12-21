a6b0f0b118bc95b6053cf9390b00e3b6158187dd
Bhargavscodde <154326629+bhargav1232000@users.noreply.github.com>
2023-12-21 09:57:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-    access_key = "AKIATSHCMGCWMF3PXR4M"
-  secret_key = "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"
+    access_key = ""
+  secret_key = ""
 }
